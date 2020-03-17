be990a672750be7a93f2bf5bd3f25f155a0a63f0
Storm Joannes <storm.joannes@gmail.com>
2020-03-17 18:11:40 +0100

    storm

--- alle gezien van een profile.py
+++ alle gezien van een profile.py
@@ -3,69 +3,114 @@
 #https://www.psycopg.org/docs/usage.html
 
 import psycopg2
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-
-#db = client['test-database']
-db = client.huwebshop
-
-dol = db.products
-col = db.profiles
-
-products = dol.find()
-profiles = col.find()
 
 conn = psycopg2.connect("dbname=data user=postgres password=postgres")
 cur = conn.cursor()
 
-for i in profiles:
-    print(i['_id'])
-    if 'recommendations' in i:
-        gezien = (i['recommendations']['viewed_before'])
-        if gezien != None and gezien != '' and gezien != []:
-            if len(gezien) > 1:
-                ha = gezien[len(gezien) - 1]
-            else:
-                ha = gezien[0]
-            print(ha)
-            ha = "'{}'".format(ha)
-            tuin = "select name from products where id = "'{}'";".format(ha)
-            cur.execute(tuin)
-            naam = cur.fetchall()
-            if len(naam) == 0:
-                continue
-            print(naam[0][0])
+
+cur. execute("select * from products")
+tot_products = cur.fetchall()
+
+cur.execute("select id from profiles")
+tot_id = cur.fetchall()
+
+def base():
+    for id in tot_id:
+        print(id)
+        verz = verzameling(id)
+        eind = prod_ophalen(verz, id)
+        insert_into_table(id, eind)
+        break
+
+
+def verzameling(id):
+        id = "'{}'".format(id[0])
+        uitv = "select prodid from profiles_previously_viewed where profid = "'{}'";".format(id)
+        cur.execute(uitv)
+        bel_prod_id = cur.fetchall()
+        return bel_prod_id
+
+def prod_ophalen(verz, id):
+    if len(verz) > 0:
+        if len(verz) > 1:
+            verz = verz[len(verz) - 1]
+        else:
+            verz = verz[0]
+        verz = "'{}'".format(verz[0])
+        uitv2 = "select name from products where id = "'{}'";".format(verz)
+        cur.execute(uitv2)
+        bel_prod_name = cur.fetchall()
+        return bel_prod_name[0][0]
     else:
-        
+        vergeleken = search_same(id)
+        return vergeleken
 
-conn = psycopg2.connect("dbname=voordeelshop user=postgres password=postgres")
-cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS all_pro;")
+def search_same(id):
+    search_verg = verge_mat(id)
+    cur.execute("select profid from profiles_previously_viewed")
+    test_id = cur.fetchall()
+    for h in test_id:
+        verge = verge_mat(h)
+        if verge[0] == search_verg[0]:
+            test = verzameling(h)
+            if len(test) > 0:
+                last = prod_ophalen(test, id)
+                return last
 
-cur.execute("CREATE TABLE all_pro (id serial PRIMARY KEY, "
-            "_ID varchar, "
-            "buids varchar,"
-            "order_latest varchar,"
-            "recommendations varchar, "
-            "previously_recommended varchar);")
-#cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-# Pass data to fill a query placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-for i in profiles:
-    cur.execute("INSERT INTO all_pro (_ID, buids, order_latest,recommendations, previously_recommended) VALUES (%s, %s, %s, %s, %s)",
-                (str(i['_id']),
-                 i['buids'] if 'buids' in i else None,
-                 i['order']['latest'] if 'order_latest' in i else None,
-                 i['recommendations']['viewed_before'] if 'recommendations' in i else None,
-                 i['previously_recommended'] if 'previously_recommended' in i else None))
-#ID, category, brand, gender, sub category, sub sub category, color, name, price
-"""
-# Query the database and obtain data as Python objects
-cur.execute("SELECT * FROM test;")
-cur.fetchone()
-(1, 100, "abc'def")
-"""
-# Make the changes to the database persistent
+def verge_mat(id):
+    id = "'{}'".format(id[0])
+    uitv = "select devicefamily, devicetype from sessions where profid = "'{}'";".format(id)
+    cur.execute(uitv)
+    tot = cur.fetchall()
+    return tot
+
+def insert_into_table(id, prod):
+    cur.execute("DROP TABLE IF EXISTS vergelijkbaar_prof;")
+
+    cur.execute("CREATE TABLE vergelijkbaar_prof (id serial PRIMARY KEY, "
+                "prof_id varchar, "
+                "vergelijkbaar varchar);")
+
+    cur.execute("select name from products;")
+    total = cur.fetchall()
+
+    verg = []
+    name = prod
+    if "'" in name:
+        name = name.split("'")
+        name = name[0] + "''" + name[1]
+    name = "'" + name + "'"
+
+    func = "select discount, targetaudience, category, subcategory from products where name = "'{}'";".format(name)
+    cur.execute(func)
+    allprod = cur.fetchall()
+
+    for d in range(0, len(total)):
+        if name.replace("'", '') in verg:
+            verg.remove(name.replace("'", ''))
+            
+        if len(verg) < 3:
+            name2 = str(total[d][0])
+            if "'" in name2:
+                name2 = name2.split("'")
+                name2 = name2[0] + "''" + name2[1]
+            name2 = "'" + name2 + "'"
+
+            func2 = "select discount, targetaudience, category, subcategory from products where name = "'{}'";".format(
+                name2)
+            cur.execute(func2)
+            allgerela = cur.fetchall()
+            if allprod == allgerela:
+                verg.append(name2.replace("'", ''))
+        else:
+            break
+
+    for last in verg:
+        cur.execute("INSERT INTO vergelijkbaar_prof (prof_id, vergelijkbaar) VALUES (%s, %s)", (id, last))
+
+
+base()
+
 conn.commit()
 
 # Close communication with the database
