738ccb7d0d0ae9d1384ef1782ac6192b25bed8ae
Storm Joannes <storm.joannes@gmail.com>
2020-03-18 19:17:23 +0100

    storm

--- read_csv.py
+++
@@ -1,5 +0,0 @@
->>> import csv
->>> with open('eggs.csv', 'rb') as csvfile:
-...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-...     for row in spamreader:
-...         print ', '.join(row)
